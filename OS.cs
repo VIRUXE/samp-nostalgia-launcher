1ace280f9584d5991264abe68c8bd17012d6d19e
VIRUXE <flavioaspereira@gmail.com>
2023-08-13 00:23:45 +0100

    Create OS.cs

---
+++ OS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace NostalgiaAnticheat {
+    internal class OS {
+        public static IEnumerable<(string FileName, string WindowTitle)> GetOpenWindows() {
+            return Process.GetProcesses()
+                .Where(p => !p.ProcessName.StartsWith("System") && !p.ProcessName.StartsWith("Idle") && p.MainWindowTitle != string.Empty)
+                .Select(p => (p.ProcessName, p.MainWindowTitle));
+        }
+    }
+}
