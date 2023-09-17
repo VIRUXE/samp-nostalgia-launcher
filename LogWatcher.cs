f16d1b50cd11f48e0cff5a9d25b286cc4403caa5
VIRUXE <flavioaspereira@gmail.com>
2023-09-17 17:13:59 +0100

    Update LogWatcher.cs

--- LogWatcher.cs
+++ LogWatcher.cs
@@ -9,6 +9,7 @@ namespace NostalgiaAnticheat {
 
         public static event EventHandler OnConnecting;
         public static event EventHandler OnConnected;
+        public static event EventHandler OnDisconnected;
 
         static LogWatcher() {
             fileSystemWatcher = new FileSystemWatcher {
@@ -28,36 +29,6 @@ namespace NostalgiaAnticheat {
             fileSystemWatcher.EnableRaisingEvents = false;
         }
 
-        private static void OnChanged(object sender, FileSystemEventArgs e) {
-            // If the file is being written to, we wait a moment to let the write finish
-            while(IsFileLocked(new FileInfo(e.FullPath))) Thread.Sleep(1000);
-
-            // Open a filestream on the chatlog file to read it
-            using FileStream fileStream = new(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-
-            // If the file length is less than the last read length, the file was recreated, reset the last read length
-            if(fileStream.Length < lastReadLength) lastReadLength = 0;
-
-            // Seek to the last read position
-            fileStream.Seek(lastReadLength, SeekOrigin.Begin);
-
-            using StreamReader reader = new(fileStream);
-
-            string line;
-
-            while((line = reader.ReadLine()) != null) {
-                if (string.IsNullOrEmpty(line)) continue; // Ignore empty lines
-
-                if (line.Contains("Connecting to 216.238.113.189:7777")) {
-                    OnConnecting?.Invoke(null, EventArgs.Empty);
-                } else if (line.Contains("Connected to")) {
-                    OnConnected?.Invoke(null, EventArgs.Empty);
-                }
-            }
-
-            lastReadLength = fileStream.Position;
-        }
-
         private static bool IsFileLocked(FileInfo file) {
             FileStream stream = null;
 
@@ -70,6 +41,41 @@ namespace NostalgiaAnticheat {
             }
 
             return false;
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e) {
+            // If the file is being written to, we wait a moment to let the write finish
+            while(IsFileLocked(new FileInfo(e.FullPath))) Thread.Sleep(1000);
+
+            // Open a filestream on the chatlog file to read it
+            using FileStream fileStream = new(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+            // If the file length is less than the last read length, the file was recreated, reset the last read length
+            if (fileStream.Length < lastReadLength) {
+            
+                lastReadLength = 0;
+            }
+
+                // Seek to the last read position
+                fileStream.Seek(lastReadLength, SeekOrigin.Begin);
+
+            using StreamReader reader = new(fileStream);
+
+            string line;
+
+            while((line = reader.ReadLine()) != null) {
+                if (string.IsNullOrEmpty(line)) continue; // Ignore empty lines
+
+                if (line.Contains("Connecting to 216.238.113.189:7777")) {
+                    OnConnecting?.Invoke(null, EventArgs.Empty);
+                } else if (line.Contains("Connected to")) {
+                    OnConnected?.Invoke(null, EventArgs.Empty);
+                } else if (line.Contains("Server Closed Connection")) {
+                    OnDisconnected?.Invoke(null, EventArgs.Empty);
+                }
+            }
+
+            lastReadLength = fileStream.Position;
         }
     }
 }
