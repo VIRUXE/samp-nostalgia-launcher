3806fd473607d37afdb0bc3bdab08c6b6222faf3
VIRUXE <flavioaspereira@gmail.com>
2023-09-17 17:13:49 +0100

    +GenerateHWID

--- OS.cs
+++ OS.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Management;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace NostalgiaAnticheat {
     internal class OS {
@@ -8,6 +11,27 @@ namespace NostalgiaAnticheat {
             return Process.GetProcesses()
                 .Where(p => !p.ProcessName.StartsWith("System") && !p.ProcessName.StartsWith("Idle") && p.MainWindowTitle != string.Empty)
                 .Select(p => (p.ProcessName, p.MainWindowTitle));
+        }
+
+        public static string GenerateHWID() {
+            var processorId = "";
+
+            // Get processor ID
+            foreach (ManagementObject mo in new ManagementObjectSearcher("Select ProcessorId From Win32_processor").Get()) {
+                processorId = mo["ProcessorId"].ToString();
+                break;
+            }
+
+            // Get volume _hwid number
+            ManagementObject dsk = new(@"win32_logicaldisk.deviceid=""c:""");
+            dsk.Get();
+
+            byte[] bytes = SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(processorId + dsk["VolumeSerialNumber"]));
+            StringBuilder builder = new();
+
+            for (var i = 0; i < bytes.Length; i++) builder.Append(bytes[i].ToString("x2"));
+
+            return builder.ToString();
         }
     }
 }
