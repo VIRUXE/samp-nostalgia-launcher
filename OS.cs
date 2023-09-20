17a809743920debb848c7369cc1c9e6d912efcb8
VIRUXE <flavioaspereira@gmail.com>
2023-09-20 23:23:20 +0100

    7zip installer

--- OS.cs
+++ OS.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Management;
+using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
+
+using HtmlAgilityPack;
 
 namespace NostalgiaAnticheat {
     internal class OS {
@@ -32,6 +38,85 @@ namespace NostalgiaAnticheat {
             for (var i = 0; i < bytes.Length; i++) builder.Append(bytes[i].ToString("x2"));
 
             return builder.ToString();
+        }
+
+        public static bool Is7ZipInstalled() {
+            try {
+                Process process = new();
+                process.StartInfo.FileName = "7z";
+                process.StartInfo.Arguments = "";
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.Start();
+
+                return true;
+            } catch {
+                return false;
+            }
+        }
+
+        public static async Task<string> Get7ZipInstallerUrl() {
+            var response = await new HttpClient().GetStringAsync("https://www.7-zip.org/download.html");
+
+            var document = new HtmlDocument();
+            document.LoadHtml(response);
+
+            int rowIndex = Environment.Is64BitOperatingSystem ? 5 : 6;
+
+            var msiUrlNode = document.DocumentNode.SelectSingleNode($"//table[@cellspacing='1']//tr[{rowIndex}]//td[@class='Item'][1]/a");
+
+            if (msiUrlNode != null)
+                return "https://www.7-zip.org/" + msiUrlNode.Attributes["href"].Value;
+            else
+                throw new Exception("Unable to find the MSI installer URL.");
+        }
+
+        public static async Task<bool> Install7Zip() {
+            try {
+                string installerUrl = await Get7ZipInstallerUrl();
+                string installerName = Path.GetFileName(new Uri(installerUrl).AbsolutePath);
+                string installerPath = Path.Combine(Directory.GetCurrentDirectory(), installerName);
+
+                if (!File.Exists(installerPath)) {
+                    Console.Write("Downloading 7Zip installer... ");
+                    try {
+                        byte[] installerBytes = await new HttpClient().GetByteArrayAsync(installerUrl);
+                        await File.WriteAllBytesAsync(installerPath, installerBytes);
+                        Console.WriteLine("Done.");
+                    } catch (Exception downloadEx) {
+                        Console.WriteLine("Failed.");
+                        Console.WriteLine("Could not download the installer: " + downloadEx.Message);
+                        return false;
+                    }
+                }
+
+                using Process installProcess = new() {
+                    StartInfo = new ProcessStartInfo {
+                        FileName = "msiexec",
+                        Arguments = $"/i \"{installerPath}\" /qn",
+                        Verb = "runas"
+                    }
+                };
+
+                Console.Write("Installing 7Zip... ");
+                installProcess.Start();
+                installProcess.WaitForExit();
+
+                if (installProcess.ExitCode != 0) {
+                    Console.WriteLine("Failed.");
+                    Console.WriteLine($"Installation failed with exit code {installProcess.ExitCode}");
+                    return false;
+                }
+
+                Console.WriteLine("Successful.");
+                return true;
+            } catch (Exception ex) {
+                Console.WriteLine("Failed.");
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return false;
+            }
         }
     }
 }
